80f653d6f3a225c84b403b5eef50ce27f6c8ad2b
Muhammad Hassam <hassamshoaib96@gmail.com>
2018-04-11 12:50:43 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,7 @@ var Mail = require('../models/mail');
 
 var productsController = require('../controllers/productController');
 
-router.get('/test/socket', function(req, res){
-    res.render('productUpdate', {});
-    //productsController.getCategoryController(req, res);
-});
 
-router.get('/test', function(req, res){
-    console.log(req.user.id);
-});
 
 router.get('/categories/:parentCategoryId', function(req, res){
     productsController.getSubCategoriesController(req, res);    
