99b14043b075d25d613d66af9983676c17663296
Muhammad Hassam <hassamshoaib96@gmail.com>
2018-04-11 01:51:41 -0700

    Port number input format modified for heroku

--- app.js
+++ app.js
@@ -93,7 +93,9 @@ app.use(function (err, req, res, next) {
     });
 });
 
-app.set('port', process.env.PORT || 3000);
+var portNo = process.env.PORT || 3000;
+
+app.set('port', portNo);
 
 var server = app.listen(app.get('port'), function () {
     debug('Express server listening on port ' + server.address().port);
