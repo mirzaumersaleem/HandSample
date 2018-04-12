d8600a2a21c0d97afbf8c207a3e0090a782d9139
Muhammad Hassam <hassamshoaib96@gmail.com>
2018-04-12 00:33:29 -0700

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 var productsController = require('../controllers/productController');
 
-router.get('/category/:subCategoryId', function(req, res){
+router.get('/category', function(req, res){
     console.log("product controller executed");
     productsController.getSubCatProductsController(req, res);
 });
 
-router.get('/product-details/:productId', function(req, res){
+router.get('/product-details', function(req, res){
     console.log("Inside product details route");
     productsController.getProductDetailsController(req, res);
 });
