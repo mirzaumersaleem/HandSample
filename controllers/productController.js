8d0c1e39a5f770df4f92fcdcb236d56713b9b77b
Muhammad Hassam <hassamshoaib96@gmail.com>
2018-04-12 11:51:46 -0700

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,31 @@
-﻿var mySql = require("../config/database");
+var mySql = require("../config/database");
 
 var category = require("../models/category");
 var product = require("../models/product");
+
+/*
+    This is an asynchronous function that fetch subcategories
+    for each parent category. THe fetching of sub categories is synchronous
+*/
+async function getMainAndSubCat(parentCategories){
+    
+    //Return a promise when all subcategories are fetched for parent categories
+    return new Promise(async function(resolve){
+        var catMainAndSub = []; //Array to save parent and their sub categories
+        let categories1 = new category();   //Category class object
+        for(i = 0; i < parentCategories.length; i++){
+                var subCategories = await categories1.getSubCatPromise(parentCategories[i].id); //The execution would wait until subcategories are fetched
+                //Populating array with parent and subcategories
+                catMainAndSub.push({
+                                "parentCategory": parentCategories[i],
+                                "childCategories": subCategories
+                });
+        }
+        resolve(catMainAndSub); //Returning parent and subcategories when every thing executes correctly
+    });
+
+    
+}
 
 /*
     This controller returns all the parent categories
@@ -30,23 +54,21 @@ exports.getCategoryController = function(req, res) {
     return all the ssub categories that are in the parent
     category
  */
-exports.getSubCategoriesController = function(req, res){
+exports.getAllCategoriesController = function(req, res){
     var categories = new category();
-    categories.getSubCategories(req.params.parentCategoryId, function(err, result){
 
-        if(err){
-            res.json({
-                status: 500,
-                message: err
-            });
-        } else{
-            res.json({
-                status: 200,
-                data: result
-            });
-        }
-
+    categories.getCategories(async function(err, result){
+        //Get parent categories
+        var parentCategories = result;
+        //Now this function will call another async function and await to get the result
+        var catAndTheirSubCat = await getMainAndSubCat(parentCategories);   //execution awaiting until all parent and subcategories are fetched
+        console.log("Outside loop");
+        res.json({
+            status:200,
+            data: catAndTheirSubCat
+        });
     });
+    
 }
 
 /*
@@ -55,7 +77,7 @@ exports.getSubCategoriesController = function(req, res){
  */
 exports.getSubCatProductsController = function(req, res){
     var products = new product();
-    products.getSubCatProd(req.params.subCategoryId, function(err, result){
+    products.getSubCatProd(req.query.subCategoryId, function(err, result){
         if(err){
             res.json({
                 status: 500,
@@ -67,8 +89,6 @@ exports.getSubCatProductsController = function(req, res){
                 data: result
             });
         }
-        console.log(result);
-        res.render('index', {title: "Express"});
     });
 }
 
@@ -77,8 +97,9 @@ exports.getSubCatProductsController = function(req, res){
     and returns all the details of that product
  */
 exports.getProductDetailsController = function(req, res){
-    var products = product();
+    var products = new product();
 
+    console.log("Product id entered " + req.query.productId);
     products.getProductDetails(req.params.productId, function(err, result){
         if(err){
             res.json({
