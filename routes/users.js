618fc84c1dc00611be3a713fecd5248ddc3ed67f
Muhammad Hassam <hassamshoaib96@gmail.com>
2018-04-15 14:22:36 -0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,4 @@
-﻿'use strict';
+'use strict';
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
@@ -13,21 +13,103 @@ router.get('/register', function (req, res) {
     res.render('signup', {});
 });
 
+router.post('/register', function(req, res, next){
+    passport.authenticate('local-register', function(err, user, info){
+    console.log("Inside user-register post route");
+        
+        if(err){
+            return res.json({
+                status:500,
+                message: err
+            });
+
+        }
+        if(info.message[0] == 422){
+            return res.json({
+                status: 422,
+                message: info.message[1]
+            })
+        }
+        if (!user) { 
+            return res.json({
+                status:500,
+                message: info.message
+            });
+        }
+
+        req.logIn(user, function(err){
+            if(err){
+                return res.json({
+                    status: 500,
+                    message: err
+                });
+            }
+            return res.json({
+                status:200,
+                message: "User successfully logged in",
+                user: user
+            })
+        });
+    })(req, res, next);
+});
+
+/*
 router.post('/register', passport.authenticate('local-register',{
     successRedirect: '/users/verification',
-    failureRedirect: '/users/register',
+    failureRedirect: '/users/verification',
     failureFlash: false
 }));
-
+*/
 router.get('/signin', function(req, res){
     res.render('signin', {});
 });
 
-router.post('/signin', isVerified, passport.authenticate('local-signin', {
-    successRedirect: '/',
-    failureRedirect: '/users/signin',
-    failureFlash: false
-}));
+router.post('/signin', function(req, res, next){
+    passport.authenticate('local-signin', function(err, user, info){
+        if (err) {
+            return res.json({
+                status: 500,
+                message: err
+            });
+        }
+        
+        if(info.message[0] == 422){
+            return res.json({
+                status: 422,
+                message: info.message[1]
+            })
+        }
+        
+        if (!user) {
+            return res.json({
+                status: 500,
+                message: info.message
+            });
+        }
+
+        req.logIn(user, function(err){
+            if(err){
+                return res.json({
+                    status: 500,
+                    message: err
+                });
+            }
+            return res.json({
+                status:200,
+                message: "User successfully logged in",
+                user: req.user
+            })
+        });
+    })(req, res, next);
+});
+
+router.get('/logout', function(req, res){
+    req.logout();
+    res.json({
+        status:200,
+        message: "User logged out successfully"
+    });
+});
 
 router.get('/addresses', function(req, res){
     userController.getUserAddressController(req, res);    
@@ -117,11 +199,37 @@ router.get('/verification', function(req, res){
     res.render('verification', {});
 });
 
-router.post('/verification', verificationMiddleWare, passport.authenticate('local-signin', {
-    successRedirect: '/',
-    failureRedirect: '/users/signin',
-    failureFlash: false
-}));
+router.post('/verification', verificationMiddleWare, function(req, res, next){
+    console.log("Inside verification post");
+    passport.authenticate('local-signin', function(err, user, info){
+        console.log("Inside passport authenticate callback");
+        if (err) {
+            return res.json({
+                status: 500,
+                message: err
+            });
+        }
+        if (!user) {
+            return res.json({
+                status: 500,
+                message: info.message
+            });
+        }
+
+        req.logIn(user, function(err){
+            if(err){
+                return res.json({
+                    status: 500,
+                    message: err
+                });
+            }
+            return res.json({
+                status:200,
+                message: "User successfully logged in"
+            })
+        });
+    })(req, res, next);
+});
 
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
@@ -181,7 +289,10 @@ function isVerified(req, res, next){
             next();
         }
         else{
-            res.redirect('/users/verification');
+            res.json({
+                status:500,
+                message: "User Not Verified"
+            });
         }
     });
 }
