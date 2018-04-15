ac34b5cea471f7a7616e5cee02b86ec518ed591d
Muhammad Hassam <hassamshoaib96@gmail.com>
2018-04-15 14:36:27 -0700

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -27,65 +27,87 @@ passport.use('local-register', new localStrategy({
     passwordFiled: 'password',
     passReqToCallback: true
 }, function(req, username, password, done){
+            console.log("inside errors1");
+  
+    req.checkBody("name", "Enter a valid user name").matches(/^[a-zA-Z\s]*$/).notEmpty();
+    req.checkBody("password", "Enter a valid password").notEmpty();
+    req.assert("email", "Enter a valid email").isEmail().notEmpty();
+    req.assert("mobile", "Enter a valid mobile no").matches(/^[0-9]*$/);
+    req.assert("companyNumber", "Enter a valid company number").matches(/^[0-9]*$/);
+    req.assert("company", "Enter a valid company name").matches(/^[a-zA-Z\s]*$/).notEmpty();
+    console.log("Inside errors2");
     
-    var user = new User(); 
+    var error = req.validationErrors(true);
 
-    user.findByEmail(username, function(err, resultUser){
+    var errorValues = Object.keys(error);
     
-        if(err){
-            console.log(err);
-            return done(err);
-        }
-        if(resultUser.length === 1){
-            return done(null, false, {message: "Email id already in use"});
-        }
+    console.log("error length " + errorValues.length);
 
-        //Generate a random number between 1 and 10000 and assign it as a verification
-        var verificationCode = Math.floor((Math.random() * 10000) + 1); 
-        //Data of the new user
-        var newUser = {
-                name: req.body.name,
-                mobile: req.body.mobile,
-                fax: req.body.fax,
-                email: req.body.email,
-                password: user.generatePasswordHash(req.body.password),
-                verificationCode: verificationCode,
-                verificationStatus: false,
-                company: req.body.company,
-                companyNumber: req.body.companyNumber
-        }        
+    if(errorValues.length > 0){
+        console.log("inside if");
+        done(null, false, {message: [422, error]});
+    }
+    else{
+        var user = new User(); 
 
-        //Creating new user
-        user.setNewUser(newUser, function(err, newAddedUser){
+        user.findByEmail(username, function(err, resultUser){
+        
             if(err){
-                console.log("There is an error ");
                 console.log(err);
                 return done(err);
-            } else{
-                //Callback function returned with the new user created
-                console.log(newAddedUser);
-                console.log("Inside else block");
-                //Send verification code in email after the user is created
-                var mail = new Mail();
-                var emailContent = "Welcome to Sadaliah. Please user the following verification code = " + verificationCode +
-                            " on your next login. Thanks";
-                //Instantiating mail transporter that will send mail to customers
-                var transporter = mail.getTransporter("gmail", "sadaliahiksaudi@gmail.com", "SadaliaH789");
-                //Sending mail using the instantiated transporter
-                mail.sendMail(newUser.email, "sadaliahiksaudi@gmail.com", "Sadaliah Verification Code", emailContent, transporter);
-                //Set the authentication to false since user has to verify it registration
-                //done(null, false);
-          /*      res.json({
-                    status: 200,
-                    message: "User registered successfully.\nA verification code has been sent to email " + newUser.email
-                })*/
-               
-               // return done(null, false);
-                return done(null, newAddedUser[0]);
+            }
+            if(resultUser.length === 1){
+                return done(null, false, {message: "Email id already in use"});
             }
 
-        })
-    });
+            //Generate a random number between 1 and 10000 and assign it as a verification
+            var verificationCode = Math.floor((Math.random() * 10000) + 1); 
+            //Data of the new user
+            var newUser = {
+                    name: req.body.name,
+                    mobile: req.body.mobile,
+                    fax: req.body.fax,
+                    email: req.body.email,
+                    password: user.generatePasswordHash(req.body.password),
+                    verificationCode: verificationCode,
+                    verificationStatus: false,
+                    company: req.body.company,
+                    companyNumber: req.body.companyNumber
+            }        
+
+            //Creating new user
+            user.setNewUser(newUser, function(err, newAddedUser){
+                if(err){
+                    console.log("There is an error ");
+                    console.log(err);
+                    done(err);
+                } else{
+                    //Callback function returned with the new user created
+                    console.log(newAddedUser);
+                    console.log("Inside else block");
+                    //Send verification code in email after the user is created
+                    //var mail = new Mail();
+                    //var emailContent = "Welcome to Sadaliah. Please user the following verification code = " + verificationCode +
+                      //          " on your next login. Thanks";
+                    //Instantiating mail transporter that will send mail to customers
+                    //var transporter = mail.getTransporter("gmail", "sadaliahiksaudi@gmail.com", "SadaliaH789");
+                    //Sending mail using the instantiated transporter
+                    //mail.sendMail(newUser.email, "sadaliahiksaudi@gmail.com", "Sadaliah Verification Code", emailContent, transporter);
+                    //Set the authentication to false since user has to verify it registration
+                    //done(null, false);
+            /*      res.json({
+                        status: 200,
+                        message: "User registered successfully.\nA verification code has been sent to email " + newUser.email
+                    })*/
+                
+                // return done(null, false);
+                    done(null, newAddedUser[0]);
+                }
+
+            })
+        });
+    }
+    
     
 }));
 
@@ -96,24 +118,34 @@ passport.use('local-signin', new localStrategy({
 }, 
 function(req, username, password, done){
     console.log("Inside passport Strategy");
+
+    req.checkBody("passowrd", "Password length cannot be less 4").isLength({min: 4}).isEmpty();
+    req.assert("email", "Enter a valid email").isEmail().isEmpty();
+
+
     var user = new User();
+
+    if(errorValues.length > 0){
+        console.log("inside if");
+        return done(null, false, {message: [422, error]});
+    }
 
     user.findByEmail(username, function(err, result){
         
         if(err){
             console.log(err);
-            done(err);
+            return done(err);
         } 
         if(result[0].id === null){
             console.log("No user found");
-            done(null, false, {message: "No user found"});
+            return done(null, false, {message: "No user found"});
         }
         if(!user.validPassword(password, result[0].password)){
             console.log("Incorrect password entered");
-            done(null, false, {message: "Incorrect Password"});
+            return done(null, false, {message: "Incorrect Password"});
         }
         console.log("Every thing is correct in signin");
-        done(null, result[0]);
+            return done(null, result[0]);
     });
 
 }));
