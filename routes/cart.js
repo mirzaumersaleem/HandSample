964fb8f414c47d913add398f5b94a3cc09d6a762
Muhammad Hassam <hassamshoaib96@gmail.com>
2018-04-27 03:00:19 -0700

    Update cart.js

--- routes/cart.js
+++ routes/cart.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var cartController = require('../controllers/cartController');
 
-router.get('/addtocart/:id', function(req, res){
+router.get('/addtocart', function(req, res){
     cartController.addToCartController(req, res);
 })
 
@@ -11,12 +11,25 @@ router.get('/shopping-cart', function(req, res){
     cartController.shoppingCartController(req, res);
 });
 
-router.get('/final-checkout', function(req, res){
+/*
+router.get('/final-checkout', isLoggedIn, function(req, res){
+    cartController.finalCheckoutController(req, res);
+});
+*/
+
+router.post('/final-checkout', isLoggedIn, function(req, res){
     cartController.finalCheckoutController(req, res);
 });
 
-router.post('/final-checkout', function(req, res){
-    cartController.finalCheckoutController(req, res);
-});
-
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        next();
+    }
+    else{
+        res.json({
+            status: 200,
+            message: "User must be logged in"
+        });
+    }
+}
 module.exports = router;
